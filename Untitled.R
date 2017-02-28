d938b934c80f12d909b39534f1e0ce0908f68040
Klemen Voncina <klemenvon@gmail.com>
2017-02-28 10:55:52 +0100

    Initial File

---
+++ Untitled.R
@@ -0,0 +1,11 @@
+### Research Methods Assignment 3
+library(WHO)
+library(dplyr)
+
+## Plot devices (funny joke...)
+library(stringr)
+library(ggplot2)
+
+## Download codes
+codes <- get_codes()
+View(codes)
